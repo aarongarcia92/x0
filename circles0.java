2db85d3ead84312cba21188d9545f60eb3cbf60e
aarongarcia92 <Aarongarcia1292@yahoo.com>
2015-09-29 23:20:05 -0400

    Update circles0.java
    changed sizes as well as x,y

--- circles0.java
+++ circles0.java
@@ -2,10 +2,11 @@
 
 // Setup:  screen size. //
 void setup() {
-  size(200, 200);                // Try changing the width & height.
+  size(150, 150);                // Try changing the width & height.
 }
 
 // Next frame:  circle at (x,y). //
 void draw() {
-  ellipse( 50,50, 30,30 );       // Try changing the (x,y) and size.
+  ellipse( 40,40, 60,60 );  
+  size(200,200);// Try changing the (x,y) and size.
 }
